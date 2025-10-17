6e8127ab55e49441c5f7e8db8bdcf4b06364de4d
Zaaynab10 <116828252+Zaaynab10@users.noreply.github.com>
2025-10-17 11:01:03 +0200

    refactor server.js to improve dotenv configuration and streamline port handling

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-import dotenv from "dotenv";  
-dotenv.config();              
-import './db.js'
-
 import express from "express";
 import todoRoutes from "./src/routes/TodoRoutes.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
+const port = process.env.PORT || 3000;
+
 app.use(express.json());
 app.use("/todos", todoRoutes);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`✅ Serveur démarré sur le port ${PORT}`));
+app.listen(port, () => {
+  console.log(` Serveur démarré sur le port ${port}`);
+});
