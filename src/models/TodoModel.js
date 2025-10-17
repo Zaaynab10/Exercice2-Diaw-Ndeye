5a306feb5c3997db5beafd52f7e0b7df2bd347b3
Zaaynab10 <116828252+Zaaynab10@users.noreply.github.com>
2025-10-17 11:02:13 +0200

    refactor TodoModel to use SQL queries and remove mongoose dependency

--- src/models/TodoModel.js
+++ src/models/TodoModel.js
@@ -1,41 +1,58 @@
-import mongoose from 'mongoose';
+import connexion from "../../db.js";
 
-const schema = new mongoose.schema({
-title:{type:String , required:true , default:"Mettre mes taches"},
-completed:{type:Boolean , default:false}
-});
-
-const todos = mongoose.model('todos', schema );
-
- export async function getAll(){
- const tasks = await todos.find();
-return tasks ;
-}
-
-
-/*
-
-export default class Todo {
+export default class TodoModel {
   constructor() {
-    this.todos = [];
-    this.id = 1;
-  }
-  getAll() {
-    return this.todos;
-  }
-  addTask(title) {
-    const newTask = { id: this.id++, title, done: false };
-    this.todos.push(newTask);
-    return newTask;
+    this.tasks = [];
   }
 
-  deleteTask(id) {
-    const index = this.todos.findIndex((t = t.id == id));
-    if (index !== -1) {
-      return this.todos.splice(index, 1)[0];
+  async getAll() {
+    try {
+      const query = `SELECT * FROM tasks ORDER BY id`;
+      const result = await connexion.query(query);
+      this.tasks = result.rows;
+      return this.tasks;
+    } catch (error) {
+      console.error("Erreur TodoModel.getAll:", error);
+      throw error;
     }
-    return nulll;
+  }
+
+  
+async create(title) {
+  try {
+    const query = `INSERT INTO tasks (title) VALUES ($1) RETURNING *`;
+    const values = [title];
+    const result = await connexion.query(query, values);
+    return result.rows[0]; 
+  } catch (error) {
+    console.error("Erreur TodoModel.create:", error);
+    throw error;
   }
 }
-*/
 
+async completed(status, id) {
+  try {
+    const query = "UPDATE tasks SET status = $1 WHERE id = $2 RETURNING *";
+    const values = [status, id];
+    const result = await connexion.query(query, values);
+    return result.rows[0]; 
+  } catch (error) {
+    console.error("Erreur TodoModel.completed:", error);
+    throw error;
+  }
+}
+
+  async delete(id) {
+  try {
+    const query = "DELETE FROM tasks WHERE id = $1 RETURNING *";
+    const values = [id];
+    const result = await connexion.query(query, values);
+    const task = result.rows[0];
+    return task;
+  } catch (error) {
+    console.error("Erreur TodoModel.delete:", error);
+    throw error;
+  }
+}
+
+}
