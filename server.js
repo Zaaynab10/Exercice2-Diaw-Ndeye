96510ec021ca84d1a9229fef9227b65074a76d38
Zaaynab10 <116828252+Zaaynab10@users.noreply.github.com>
2025-10-17 15:54:40 +0200

    `Refactor seerver.js : switch from PostgreSQL to MongoDB`

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 import express from "express";
 import todoRoutes from "./src/routes/TodoRoutes.js";
 import dotenv from "dotenv";
+import connectDB from "./db.js";
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
-
+connectDB();
 app.use(express.json());
 app.use("/todos", todoRoutes);
+app.set('view engine', 'ejs');
+app.use(express.static('view')); 
 
 app.listen(port, () => {
   console.log(` Serveur démarré sur le port ${port}`);
