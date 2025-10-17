50b3ca2cf5e606258112cacff487e95e4f40ea68
Zaaynab10 <116828252+Zaaynab10@users.noreply.github.com>
2025-10-17 11:02:48 +0200

    refactor TodoRoutes to use instance methods from TodoController

--- src/routes/TodoRoutes.js
+++ src/routes/TodoRoutes.js
@@ -2,9 +2,11 @@ import express from "express";
 import TodoController from "../controllers/TodoController.js";
 
 const router = express.Router();
+const todoController = new TodoController();
 
-router.get("/", TodoController.list);
-router.post("/", TodoController.add);
-router.delete("/:id", TodoController.delete);
+router.get("/", todoController.getAll);
+router.post("/", todoController.create);
+router.put("/:id", todoController.completed);
+router.delete("/:id", todoController.delete);
 
 export default router;
