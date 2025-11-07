371d2e5a67b4610e8ec8dcb9f98b7276893bdf4e
Zaaynab10 <116828252+Zaaynab10@users.noreply.github.com>
2025-11-07 10:00:43 +0100

    Update db.js to support connections to both PostgreSQL and MongoDB

--- db.js
+++ db.js
@@ -1,12 +1,33 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
+const { Pool } = require("pg");
+const dotenv = require("dotenv");
 
+dotenv.config();
 
-export  default async function connectDB() {
+let pool; 
+
+async function connectDB() {
+  const type = process.env.DB_TYPE;
+
   try {
-    const db = await mongoose.connect(process.env.DATABASE_URL);
-    console.log("✅ Connexion à MongoDB réussie !");
-    return db;
+    if (type === "SQL") {
+      console.log("Connexion à PostgreSQL...");
+      pool = new Pool({
+        connectionString: process.env.DATABASE_URL,
+      });
+      await pool.connect();
+      console.log("Connexion SQL réussie !");
+      return pool;
+    } else if (type === "NOSQL") {
+      console.log("Connexion à MongoDB...");
+      const db = await mongoose.connect(process.env.MONGO_URL);
+      console.log("Connexion NoSQL réussie !");
+      return db;
+    }
   } catch (error) {
-    console.error("❌ Erreur de connexion à MongoDB :", error.message);
+    console.error("Erreur de connexion :", error);
+    process.exit(1);
   }
 }
+
+module.exports = { connectDB, pool };
