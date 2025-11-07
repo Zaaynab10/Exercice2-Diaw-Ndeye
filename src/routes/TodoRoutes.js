ca1849a13e7d6e854a3cfd1696f4a26a393693b5
Zaaynab10 <116828252+Zaaynab10@users.noreply.github.com>
2025-11-07 10:10:11 +0100

    Refactor TodoRoutes.js to use CommonJS requires and integrate Swagger UI for API documentation

--- src/routes/TodoRoutes.js
+++ src/routes/TodoRoutes.js
@@ -1,12 +1,18 @@
-import express from "express";
-import TodoController from "../controllers/TodoController.js";
+const express = require('express');
+const TodoController = require('../controllers/TodoController');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
+
 
 const router = express.Router();
 const todoController = new TodoController();
+
+router.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 
 router.get("/", todoController.getAll);
 router.post("/", todoController.create);
 router.put("/:id", todoController.completed);
 router.delete("/:id", todoController.delete);
 
-export default router;
+module.exports=router;
